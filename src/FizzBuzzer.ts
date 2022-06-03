53ec346065637e19a6eff5cb202b3200b39f68ee
Davina <nmbiya1@gmail.com>
2022-06-03 12:20:58 -0400

    check

--- src/FizzBuzzer.ts
+++ src/FizzBuzzer.ts
@@ -1,6 +1,32 @@
 export default class FizzBuzzer {
-    constructor() {}
+
+    public static MAX_VALUE = 100;
+
+    constructor() {
+    }
+
     static result(num : number) {
         return 0;
+    }
+
+    public getResult(val: number):number|string|Error {
+
+        if (val<1 || val>FizzBuzzer.MAX_VALUE) {
+            throw new Error('Invalid input');
+        }
+
+        if ((val % 3) !== 0 && (val % 5) !== 0) {
+            return val;
+        }
+
+        if ((val % 3) === 0 && (val % 5) !== 0) {
+            return "Fizz";
+        }
+
+        if ((val % 3) !== 0 && (val % 5) === 0) {
+            return "Buzz";
+        }
+
+        return "FizzBuzz";  
     }
 } 
--- src/__tests__/FizzBuzzer.test.ts
+++ src/__tests__/FizzBuzzer.test.ts
@@ -1,7 +1,35 @@
 import FizzBuzzer from "../FizzBuzzer";
 
+const fizzBuzz = new FizzBuzzer();
+
 describe('FizzBuzzer tests', () => {
     it('should return 0 if passed 0', () => {
         expect(FizzBuzzer.result(0)).toEqual(0)
     });
+
+    it('should create an instance with array', () => {
+        expect(fizzBuzz).not.toBeNull();
+    })
+
+    it('should return number', () => {
+        expect(fizzBuzz.getResult(7)).toEqual(7);
+    })
+
+    it('should return fizz', () => {
+        expect(fizzBuzz.getResult(6)).toEqual("Fizz");
+    })
+
+    it('should return buzz', () => {
+        expect(fizzBuzz.getResult(25)).toEqual("Buzz");
+    })
+
+    it('should return fizzbuzz', () => {
+        expect(fizzBuzz.getResult(30)).toEqual("FizzBuzz");
+    })
+
+    it('should throw error', () => {
+        expect(()=> {
+            fizzBuzz.getResult(FizzBuzzer.MAX_VALUE+1);
+        }).toThrowError('Invalid input');
+    })
 });
